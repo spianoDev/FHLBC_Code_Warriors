c8604426aebddf3a8cd73cd0d5c12613c0820c5e
spiano <lafleurpiano@gmail.com>
2022-01-31 08:38:07 -0600

    completed feb 1 challenge in python

---
+++ 8kyu/020122.py
@@ -0,0 +1,22 @@
+# https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/python
+# Level 8kyu
+
+## Directions ##
+
+# Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+# For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter;
+# and month 11 (November), is part of the fourth quarter.
+
+## Function ##
+
+def quarter_of(month):
+    return 1 if month <= 3 else 2 if month <= 6 else 3 if month <= 9 else 4
+
+## Test Cases ##
+
+# quarter_of(3) # => 1
+# quarter_of(8) # => 3
+quarter_of(11) # => 4
+
+
