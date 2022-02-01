038d1af81d8c5f47a9f6c9b84812e129ddc4ea1c
spiano <lafleurpiano@gmail.com>
2022-02-01 09:03:48 -0600

    completed feb 2 challenge in python

---
+++ 8kyu/020222.py
@@ -0,0 +1,18 @@
+# https://www.codewars.com/kata/551b4501ac0447318f0009cd
+# Level 8kyu
+
+## Directions ##
+
+# Implement a function which convert the given boolean value into its string representation.
+
+## Function ##
+
+def boolean_to_string(b):
+  return "True" if b == True else "False"
+
+## Test Cases ##
+
+# boolean_to_string(True) # => "true"
+boolean_to_string(False) # => "false"
+
+
