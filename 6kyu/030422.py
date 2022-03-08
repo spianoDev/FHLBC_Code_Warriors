af42adc76237a3e9b29eaf29e79e24c38cf3d217
Sarah Panaligan <spianodev@gmail.com>
2022-03-07 19:45:37 -0600

    completed mar 4 but it is not perfect

--- 6kyu/030422.py
+++ 6kyu/030422.py
@@ -3,16 +3,16 @@
 
 ## Directions ##
 
-# Write a function that takes a string of braces, and determines if the order of the braces is valid.
-# It should return true if the string is valid, and false if it's invalid.
+# Write a function that takes a str of braces, and determines if the order of the braces is valid.
+# It should return true if the str is valid, and false if it's invalid.
 #
 # This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}.
 # Thanks to @arnedag for the idea!
 #
-# All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
+# All input strs will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
 #
 # What is considered Valid?
-# A string of braces is considered valid if all braces are matched with the correct brace.
+# A str of braces is considered valid if all braces are matched with the correct brace.
 #
 # Examples
 # "(){}[]"   =>  True
@@ -23,27 +23,51 @@
 
 ## Function ##
 
-def valid_braces(string):
-    try:
-        return eval(string)
-    except TypeError:
-        print('it is ok')
-        return True
-    except SyntaxError as err:
-        print(str(err))
-        # except ValueError as e:  # as e syntax added in ~python2.5
-        if str(err) != "invalid syntax (<string>, line 1)":
-            print('tr8e')
-            return True
-        else:
-            print("caught!")
-            return False
-        # if err == 'invalid syntax (<string>, line1)':
-        #     print('error found')
-            # return False
-    # else:
-    #     print('true')
-    #     return True
+# def valid_braces(str):
+#     try:
+#         return eval(str)
+#     except TypeError:
+#         print('it is ok')
+#         return True
+#     except SyntaxError as err:
+#         print(str(err))
+#         # except ValueError as e:  # as e syntax added in ~python2.5
+#         if str(err) != "invalid syntax (<str>, line 1)":
+#             print('tr8e')
+#             return True
+#         else:
+#             print("caught!")
+#             return False
+#         # if err == 'invalid syntax (<str>, line1)':
+#         #     print('error found')
+#             # return False
+#     # else:
+#     #     print('true')
+#     #     return True
+
+def valid_braces(str):
+    brace_options = {
+        "{": "}",
+        "[": "]",
+        "(": ")"
+    }
+    while len(str) > 1:
+        if len(str) > str.find("{") >= 0:
+            if str.find("}") != -1 and str.find("}") > str.find("{"):
+                str = str[:str.find("{")] + str[str.find("}")+1:]
+            else:
+                return False
+        elif len(str) > str.find("[") >= 0:
+            if str.find("]") != -1 and str.find("]") > str.find("["):
+                str = str[:str.find("[")] + str[str.find("]")+1:]
+            else:
+                return False
+        elif len(str) > str.find("(") >= 0:
+            if str.find(")") != -1 and str.rfind(")") > str.find("("):
+                str = str[:str.find("(")] + str[str.rfind(")")+1:]
+            else:
+                return False
+    return len(str) == 0
 
 
 
@@ -56,9 +80,9 @@ def valid_braces(string):
 # valid_braces( "{}" ) # => True
 # valid_braces( "{}()[]" ) # => True
 # valid_braces( "([{}])" ) # => True
-# valid_braces( "([}{])" ) # => False
+valid_braces( "([}{])" ) # => False
 # valid_braces( "{}({})[]" ) # => True
 # valid_braces( "(({{[[]]}}))" ) # => True
-valid_braces( "(((({{" ) # => False
+# valid_braces( "(((({{" ) # => False
 # valid_braces( ")(}{][" ) # => False
 # valid_braces( "())({}}{()][][" ) # => False
